2124f7a66ed7373affd84b67c0c28eb7f47f9504
DESKTOP-Maria <mariaivanuss@gmail.com>
2025-01-23 18:12:16 +0200

    add menu placeholder

---
+++ Bubble-Civilization/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    //[SerializeField] GameObject ActualGame;
+    [SerializeField] AudioSource AudioSourceGO;
+    [SerializeField] Slider volumeSlider;
+
+    //private GameObject MainMenuGO;
+
+    public void menuStrart()
+    {
+        
+        this.gameObject.SetActive(false);
+        //ActualGame.SetActive(true);
+    }
+
+    public void changeVolume()
+    {
+        AudioSourceGO.volume = volumeSlider.value;
+    }
+
+
+    public void menuQuit()
+    {
+        Application.Quit();
+    }
+
+}
